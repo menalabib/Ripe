69a98032956f611bcbc3abc4f3f182ba944ddc18
menalabib <menalabib@ymail.com>
2019-07-19 20:24:03 -0400

    working get_content

--- RipeBE/src/server/RipeServer.py
+++ RipeBE/src/server/RipeServer.py
@@ -3,6 +3,7 @@ import os
 from azure.storage.blob import BlockBlobService, PublicAccess
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from random import shuffle
 from werkzeug.exceptions import BadRequest
 from werkzeug.utils import secure_filename
 
@@ -18,6 +19,7 @@ UPLOADED_BY = 'uploaded_by'
 UPVOTES = 'upvotes'
 DOWNVOTES = 'downvotes'
 VIEWS = 'views'
+IS_VIDEO = 'is_video'
 SCORE = 'score'
 ACTION = 'action'
 NAME = 'name'
@@ -70,6 +72,7 @@ def post_content():
         UPVOTES: 0,
         DOWNVOTES: 0,
         VIEWS: 0,
+        IS_VIDEO: int(request.form[IS_VIDEO]),
     }
 
     post_id = content_collection.insert_one(content).inserted_id
@@ -91,10 +94,10 @@ def post_content():
                                              os.path.join(constants.paths['UPLOAD_FOLDER'], filename))
 
     # List the blobs in the container.
-    print("\nList blobs in the container")
-    generator = block_blob_service.list_blobs(constants.blob_consts['CONTAINER_NAME'])
-    for blob in generator:
-        print("\t Blob name: " + blob.name)
+    # print("\nList blobs in the container")
+    # generator = block_blob_service.list_blobs(constants.blob_consts['CONTAINER_NAME'])
+    # for blob in generator:
+    #     print("\t Blob name: " + blob.name)
 
     print(post_id)
     return jsonify(str(post_id))
@@ -102,9 +105,13 @@ def post_content():
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
+    email = request.form[EMAIL]
+    user = user_collection.find_one({EMAIL: email})
+    if user is not None:
+        return jsonify(user[UUID])
     user = {
         NAME: request.form[NAME],
-        EMAIL: request.form[EMAIL],
+        EMAIL: email,
         CONTENT_UPLOADED: [],
         SCORE: 0,
         SAVED_CONTENT: [],
@@ -114,7 +121,6 @@ def create_user():
     post_id = user_collection.insert(user)
     user_collection.update_one({EMAIL: request.form[EMAIL]}, {'$set': {UUID: str(post_id)}})
 
-    print(str(post_id))
     return jsonify(str(post_id))
 
 
@@ -126,43 +132,51 @@ def get_content_for_user(user_uid):
         raise BadRequest('User Not Found!')
 
     user_tags = user_dict[TAGS]
+    shuffle(user_tags)
     viewed_content = user_dict[VIEWED_CONTENT]
     content_uploaded = user_dict[CONTENT_UPLOADED]
-    user_content = []
-    return_list = []
+    # user_content = []
+    # return_list = []
 
     for tag in user_tags:
-        user_content += list(content_collection.find({TAGS: tag}))
+        user_content = content_collection.find_one({TAGS: tag})
 
-    for user_cont in user_content:
-        if str(user_cont.get('_id')) not in return_list and str(user_cont.get('_id')) not in viewed_content \
-                and str(user_cont.get('_id')) not in content_uploaded:
-            return_list.append(str(user_cont.get('_id')))
+        # for user_cont in user_content:
+        if str(user_content.get('_id')) not in viewed_content and str(user_content.get('_id')) not in content_uploaded:
+            return jsonify([user_content.get(UID), user_content.get(TITLE), user_content.get(IS_VIDEO)])
 
-    if return_list is None:
-        for all_cont in list(content_collection.find()):
-            if all_cont not in viewed_content:
-                return_list.append()
+    # for all_cont in list(content_collection.find()):
+    #     if all_cont not in viewed_content:
+    #         return_list.append()
+    # return jsonify(return_list)
+    user_content = list(content_collection.aggregate([
+        {"$match": {"tags": {"$exists": True}}},
+        {"$sample": {"size": 3}}
+    ]))
+    for content in user_content:
+        if str(content.get('_id')) not in viewed_content and str(content.get('_id')) not in content_uploaded:
+            user_collection.update_one({UUID: user_uid}, {'$push': {VIEWED_CONTENT: content.get(UID)}})
+            return jsonify([content.get(UID), content.get(TITLE), content.get(IS_VIDEO)])
 
-    print(return_list)
-
-    return jsonify(return_list)
+    return jsonify(['No Content Available', '', '0'])
 
 
 @app.route('/update_content_view/<user_uid>/<content_uid>', methods=['PUT'])
 def update_content_views(user_uid, content_uid):
     if ACTION in request.form:
         content_dict = content_collection.find_one({UID: content_uid})
+        user_dict = user_collection.find_one({UUID: user_uid})
         content_collection.update_one({UID: content_uid}, {'$inc': {VIEWS: 1}})
-        user_collection.update_one({UUID: user_uid}, {'$push': {VIEWED_CONTENT: content_uid}})
         if request.form[ACTION] == '0':
             content_collection.update_one({UID: content_uid}, {'$inc': {DOWNVOTES: 1}})
         elif request.form[ACTION] == '1':
             content_collection.update_one({UID: content_uid}, {'$inc': {UPVOTES: 1}})
             user_collection.update_one({CONTENT_UPLOADED: content_uid}, {'$inc': {SCORE: 1}})
-            for tag in content_dict[TAGS]:
-                user_collection.update_one({UUID: user_uid}, {'$push': {TAGS: tag}})
-            user_collection.update_one({UUID: user_uid}, {'$push': {SAVED_CONTENT: content_uid}})
+            for tag in content_dict.get(TAGS):
+                if tag not in user_dict.get(TAGS):
+                    user_collection.update_one({UUID: user_uid}, {'$push': {TAGS: tag}})
+            if content_uid not in user_dict[SAVED_CONTENT]:
+                user_collection.update_one({UUID: user_uid}, {'$push': {SAVED_CONTENT: content_uid}})
 
         return "Content Updated!"
 
@@ -185,7 +199,6 @@ def get_user_by_id(user_uid):
     if user_dict is None:
         raise BadRequest('User Not Found!')
     user_dict['uuid'] = str(user_dict.get('_id'))
-    print(user_dict['_id'])
     del user_dict['_id']
     return jsonify(user_dict)
 
@@ -251,7 +264,6 @@ def post_to_group(group_uid):
     block_blob_service.create_blob_from_path(constants.blob_consts['CONTAINER_NAME'], str(post_id),
                                              os.path.join(constants.paths['UPLOAD_FOLDER'], filename))
 
-    print(post_id)
     return jsonify(str(post_id))
 
 
@@ -281,7 +293,7 @@ def update_group_content_views(user_uid, content_uid):
         group_content_collection.update_one({UID: content_uid}, {'$inc': {VIEWS: 1}})
         if request.form[ACTION] == '1':
             content_collection.update_one({UID: content_uid}, {'$inc': {UPVOTES: 1}})
-        if content_dict[UPVOTES] > content_dict[VIEWS]/2:
+        if content_dict[UPVOTES] > content_dict[VIEWS] / 2:
             group_collection.update_one({UID: group_id}, {'$push': {GALLERY: content_uid}})
         return "Content Updated!"
     raise BadRequest('Action Not Found!')
